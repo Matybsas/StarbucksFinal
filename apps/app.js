29108f230bc846d1834b5a88baf28ca7ca62aba7
Matias <matiasmastracchio@gmail.com>
2023-11-02 20:56:04 -0300

    Modificacion de Footer

--- apps/app.js
+++ apps/app.js
@@ -1,31 +1,15 @@
-//COMIENZA EL MENU
-// Al dar clic muestra el menú
-$('.open').click(function(){
-    $(this).css('display', 'none');
-    $('.close').css('display', 'block');
-   /* $('.menu').css('transform', 'translateY(125px) translateX(0)');*/
-  })
+document.getElementById("menu").addEventListener("click",function(){
   
-  // Al dar clic cierra oculta el menú
-  $('.close').click(function(){
-      $(this).css('display', 'none');
-      $('.open').css('display', 'block');
-     /* $('.menu').css('transform', 'translateY(125px) translateX(-100%)');*/
-  })
-  
-  /*
-  // Compara el ancho de la página como un media query en CSS
-  $(window).resize(() => {
-      // Restaura los valores base
-      if($(window).width() > 900){
-          $('.open').css('display', 'none');
-          $('.close').css('display', 'none');
-          $('.menu').css('transform', 'translateY(0) translateX(0)');
-      } else {
-          $('.open').css('display', 'block');
-          $('.menu').css('transform', 'translateY(125px) translateX(-100%)');
-      }
-  });
+    document.getElementsByName("navega").classList.toggle("mostrar");
+});
 
-*/
-  //FINALIZA EL MENU
+
+/*$(".mostrarLista").on( "click", function() {
+    $('.target').show(); //muestro mediante clase
+ });
+$(".mostrarLista").on( "click", function() {
+
+    $('.target').hide(); //muestro mediante clase
+});*/
+
+//FINALIZA EL MENU
